f58b9e113a89613daa58e42928d260ecfeafc175
Alexander Ben Nasrallah <me@abn.sh>
2020-03-30 08:19:55 +0200

    commit missing include.mk

---
+++ include.mk
@@ -0,0 +1,49 @@
+.SUFFIXES: .pdf .tex .lhs
+
+AGDA=agda
+TEX=xelatex
+TEXOBJ = \
+	*.aux \
+	*.bbl \
+	*.bcf \
+	*.blg \
+	*.log \
+	*.nav \
+	*.out \
+	*.pdf \
+	*.run.xml \
+	*.snm \
+	*.toc \
+	*.vrb \
+
+.tex.pdf:
+	$(TEX) $(TEXFLAGS) -jobname $* $<
+	if grep -q 'Please (re)run Biber on the file:' $*.log; \
+	then \
+		biber $*; \
+		$(TEX) $(TEXFLAGS) $<; \
+	fi
+	if grep -q 'LaTeX Warning: There were undefined references.' $*.log; then\
+		env $(TEX) $(TEXFLAGS) $<;\
+	fi
+
+.lhs.pdf:
+	$(TEX) $(TEXFLAGS) -jobname $* $<
+	if grep -q 'Please (re)run Biber on the file:' $*.log; \
+	then \
+		biber $*; \
+		$(TEX) $(TEXFLAGS) $<; \
+	fi
+	if grep -q 'LaTeX Warning: There were undefined references.' $*.log; then\
+		env $(TEX) $(TEXFLAGS) $<;\
+	fi
+
+all: $(ALL)
+
+phony:
+
+tex-clean: phony
+	rm -f $(TEXOBJ)
+
+c-clean: phony
+	rm -f *.o
